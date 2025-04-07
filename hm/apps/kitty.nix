c50fa26b6f73cd0c2d5bca1e1eca3b4fbb789677
atomiksan <25588579+atomiksan@users.noreply.github.com>
2025-04-07 14:08:39 +0530

    changed font value

--- hm/apps/kitty.nix
+++ hm/apps/kitty.nix
@@ -9,7 +9,7 @@
   programs.kitty = {
     enable = true;
     font.name = "Victor Mono Nerd Font";
-    font.size = 12;
+    font.size = 13;
     shellIntegration = {
       enableFishIntegration = true;
       enableZshIntegration = true;
