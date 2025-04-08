a78be0478d1763f8785acabb195b7acb69637191
atomiksan <25588579+atomiksan@users.noreply.github.com>
2025-04-08 06:13:32 +0530

    add direnv config to hm files

---
+++ hm/cli-apps/direnv.nix
@@ -0,0 +1,16 @@
+{
+  config,
+  pkgs,
+  ...
+}:
+
+{
+  #Direnv config
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    enableFishIntegration = true;
+  };
+}
