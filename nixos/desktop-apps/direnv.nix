e03630fb7782d2fc352cbd0a70a21f5fbc37072b
atomiksan <25588579+atomiksan@users.noreply.github.com>
2025-04-08 06:14:32 +0530

    modified direnv installation nixos

--- nixos/desktop-apps/direnv.nix
+++ nixos/desktop-apps/direnv.nix
@@ -4,12 +4,9 @@
 }: {
   #Install direnv
   programs.direnv = {
-    package = pkgs.direnv;
-    silent = false;
     loadInNixShell = true;
     nix-direnv = {
       enable = true;
-      package = pkgs.nix-direnv;
     };
   };
 }
