2b508fa785b8af1ab0b6450e937ad4c532e2cd28
atomiksan <25588579+atomiksan@users.noreply.github.com>
2025-04-30 07:52:23 +0530

    updated zshrc file to remove deprecated options

--- hm/shell-config/zsh.nix
+++ hm/shell-config/zsh.nix
@@ -1,6 +1,7 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }:
 
@@ -17,26 +18,29 @@
       ];
       useFriendlyNames = true;
     };
-    initExtraBeforeCompInit = ''
-      EDITOR=nvim
-      export GPG_TTY=$(tty)
-      export PATH=$HOME/.config/emacs/bin:$PATH
-      export PATH=$(go env GOPATH)/bin:$PATH
-      #export PATH=$HOME/zig:$PATH
-    '';
+    initContent = lib.mkMerge [
+      (lib.mkOrder 550 ''
+        EDITOR=nvim
+        export GPG_TTY=$(tty)
+        export PATH=$HOME/.config/emacs/bin:$PATH
+        export PATH=$(go env GOPATH)/bin:$PATH
+        #export PATH=$HOME/zig:$PATH
+      '')
+      (lib.mkOrder 1000 ''
+        zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+        zstyle ':completion:*' menu no
+        zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
+        zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza --color=always --icons $realpath'
+        zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza --color=always --icons $realpath'
+        eval "$(fzf --zsh)"
+        eval "$(zoxide init --cmd cd zsh)"
+        eval "$(direnv hook zsh)"
+      '')
+    ];
     completionInit = ''
       autoload -U compinit && compinit
     '';
-    initExtra = ''
-      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-      zstyle ':completion:*' menu no
-      zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
-      zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza --color=always --icons $realpath'
-      zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza --color=always --icons $realpath'
-      eval "$(fzf --zsh)"
-      eval "$(zoxide init --cmd cd zsh)"
-      eval "$(direnv hook zsh)"
-    '';
+
     autosuggestion = {
       enable = true;
     };
