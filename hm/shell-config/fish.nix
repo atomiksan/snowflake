2ee129210b4414e4a3ea3e99f2a93bec1fb4710c
atomiksan <25588579+atomiksan@users.noreply.github.com>
2025-09-17 13:44:26 +0530

    update theme rose-pine

--- hm/shell-config/fish.nix
+++ hm/shell-config/fish.nix
@@ -5,14 +5,14 @@
 }:
 
 {
-  xdg.configFile."fish/themes/Rosé Pine Dawn.theme".source = "${
+  xdg.configFile."fish/themes/Rosé Pine.theme".source = "${
     pkgs.fetchFromGitHub {
       owner = "rose-pine";
       repo = "fish";
       rev = "38aab5baabefea1bc7e560ba3fbdb53cb91a6186";
       sha256 = "bSGGksL/jBNqVV0cHZ8eJ03/8j3HfD9HXpDa8G/Cmi8=";
     }
-  }/themes/Rosé Pine Dawn.theme";
+  }/themes/Rosé Pine.theme";
 
   programs.fish = {
     enable = true;
@@ -23,7 +23,7 @@
       set -gx PATH (go env GOPATH)/bin $PATH
       set -gx PATH $HOME/.config/emacs/bin $PATH
       #set -gx PATH $HOME/zig $PATH
-      fish_config theme choose 'Rosé Pine Dawn'
+      fish_config theme choose 'Rosé Pine'
     '';
     interactiveShellInit = ''
       set -x TERM xterm-256color
