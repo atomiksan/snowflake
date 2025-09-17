b96515c23fc687f17f472f4243cfc5f29128bae7
atomiksan <25588579+atomiksan@users.noreply.github.com>
2025-09-17 13:48:02 +0530

    removed de and added greetd

--- nixos/services/xserver.nix
+++ nixos/services/xserver.nix
@@ -7,15 +7,25 @@
 
 {
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  #services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
   #services.xserver.displayManager.gdm.enable = true;
   #services.xserver.desktopManager.gnome.enable = true;
 
   # Enable the KDE plasma desktop environment
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  #services.displayManager.sddm.enable = true;
+  #services.desktopManager.plasma6.enable = true;
+
+  services.greetd = {
+     enable = true;
+     settings = {
+       default_session = {
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd Hyprland";
+	  user = "loid";
+       };
+     };
+   };
 
   # Configure keymap in X11
   services.xserver.xkb = {
