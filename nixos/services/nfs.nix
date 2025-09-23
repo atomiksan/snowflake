74c1c9d1349c70fe38fa5c3def3c9a2b7fae67fa
atomiksan <25588579+atomiksan@users.noreply.github.com>
2025-09-23 15:20:04 +0530

    open nfs ports

--- nixos/services/nfs.nix
+++ nixos/services/nfs.nix
@@ -10,4 +10,13 @@
   services.nfs.server.exports = ''
     /data/kubevirt-vm-disks *(rw,sync,no_subtree_check,no_root_squash)
   '';
+
+  networking.firewall.allowedTCPPorts = [
+    111
+    2049
+  ];
+  networking.firewall.allowedUDPPorts = [
+    111
+    2049
+  ];
 }
